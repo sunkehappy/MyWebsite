0ac78140fa6cb57e805ddff90a0fb9d35990fc6e
Calvin Sun <calvin.sun@otms.com>
2021-06-15 13:46:17 +0800

    chore: update git ignore file.

--- release.sh
+++ release.sh
@@ -2,9 +2,9 @@ cd myblog
 hugo --baseUrl="https://blog.calvinhappy.com/" -d ../nginx/public
 cd ..
 rsync -rvz ./ --exclude-from=.gitignore calvinsun@8.130.31.240:~/mysite --delete
-ssh calvinsun@8.130.31.240 > /dev/null 2>&1 << eeooff
-cd mysite
-# -d表示以守护进程来启动
-sudo docker-compose up --build -d
-eeooff
+# ssh calvinsun@8.130.31.240 > /dev/null 2>&1 << eeooff
+# cd mysite
+# # -d表示以守护进程来启动
+# sudo docker-compose up --build -d
+# eeooff
 echo done!
