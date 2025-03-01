be9547a5d48425d00fdc2a35b2043729945ce062
Calvin Sun <675313675@qq.com>
2025-03-01 22:22:45 +0800

    feat: 替换主题，现在用Next.js来静态生成网站

--- release.sh
+++ release.sh
@@ -1,9 +1,8 @@
 cd myblog
 pnpm export
-rm -rf ../nginx/out/*
-mv out/* ../nginx/out
-cd ..
-rsync -rvz ./ --exclude-from=.gitignore calvinsun@47.120.38.184:~/mysite --delete
+rm -rf ./nginx/out/*
+cp -r ./myblog/out/* ./nginx/out/
+rsync -rvz ./ --include=nginx/out --exclude-from=.gitignore calvinsun@47.120.38.184:~/mysite --delete
 # ssh calvinsun@47.120.38.184 > /dev/null 2>&1 << eeooff
 # cd mysite
 # # -d表示以守护进程来启动
