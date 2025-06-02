4edfafa1673b8f5497dff89fc39e35f225db8b75
sunke <sunke@uu898.com>
2025-06-02 16:24:47 +0800

    feat: Refine the release script

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 cd myblog
 pnpm export
+cd ..
 rm -rf ./nginx/out/*
+mkdir -p ./nginx/out // 如果已存在，则不会报错
 cp -r ./myblog/out/* ./nginx/out/
 rsync -rvz ./ --include=nginx/out --exclude-from=.gitignore calvinsun@47.120.38.184:~/mysite --delete
 # ssh calvinsun@47.120.38.184 > /dev/null 2>&1 << eeooff
